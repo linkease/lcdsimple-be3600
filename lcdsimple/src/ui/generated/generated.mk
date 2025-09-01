9cf96c045ba7203a26c9fe64bfff561e3138509e
Jason <xiaobao@linkease.com>
2025-09-01 17:16:31 +0800

    fix PRJ_DIR

--- lcdsimple/src/ui/generated/generated.mk
+++ lcdsimple/src/ui/generated/generated.mk
@@ -1,7 +1,5 @@
 # images
 
-PRJ_DIR=/home/build/istoreos-build/idisk-rk35xx/openwrt/build_dir/target-aarch64_cortex-a53_musl/lcdsimple-1.0/ui
-
 include $(PRJ_DIR)/generated/images/images.mk
 
 # GUI Guider fonts
@@ -9,7 +7,6 @@ include $(PRJ_DIR)/generated/guider_fonts/guider_fonts.mk
 
 # GUI Guider customer fonts
 include $(PRJ_DIR)/generated/guider_customer_fonts/guider_customer_fonts.mk
-
 
 GEN_CSRCS += $(notdir $(wildcard $(PRJ_DIR)/generated/*.c))
 
