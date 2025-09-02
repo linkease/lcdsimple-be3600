8b208eaf6e11ce08bca3fbf1b0c5fbc6c31d981d
Jason <xiaobao@linkease.com>
2025-09-02 14:01:07 +0800

    c924e24c7aa55317521bcd9dd75ce9337508f5a5 of lv_port_linux in https://github.com/gl-inet/gl-lvgl

---
+++ lcdsimple/src-glinet-be3600/main.c
@@ -0,0 +1,73 @@
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static const char *getenv_default(const char *name, const char *dflt)
+{
+    return getenv(name) ? : dflt;
+}
+
+#if LV_USE_LINUX_FBDEV
+static void lv_linux_disp_init(void)
+{
+    const char *device = getenv_default("LV_LINUX_FBDEV_DEVICE", "/dev/fb0");
+    lv_display_t * disp = lv_linux_fbdev_create();
+
+    lv_display_set_physical_resolution(disp, 76, 284);
+    lv_linux_fbdev_set_file(disp, device);
+    lv_display_set_rotation(disp, LV_DISPLAY_ROTATION_90);
+    lv_linux_fbdev_set_display(disp);
+}
+#elif LV_USE_LINUX_DRM
+static void lv_linux_disp_init(void)
+{
+    const char *device = getenv_default("LV_LINUX_DRM_CARD", "/dev/dri/card0");
+    lv_display_t * disp = lv_linux_drm_create();
+
+    lv_linux_drm_set_file(disp, device, -1);
+}
+#elif LV_USE_SDL
+static void lv_linux_disp_init(void)
+{
+    const int width = atoi(getenv("LV_SDL_VIDEO_WIDTH") ? : "800");
+    const int height = atoi(getenv("LV_SDL_VIDEO_HEIGHT") ? : "480");
+
+    lv_sdl_window_create(width, height);
+}
+#else
+#error Unsupported configuration
+#endif
+
+static void lv_indev_init(void)
+{
+#if LV_USE_EVDEV
+    lv_indev_t * indev = lv_evdev_create(LV_INDEV_TYPE_POINTER, "/dev/input/event0");
+    lv_evdev_set_swap_axes(indev, true);
+    lv_evdev_set_calibration(indev, 283, 0, 0, 75);
+#endif
+}
+
+int main(void)
+{
+    lv_init();
+
+    /*Linux display device init*/
+    lv_linux_disp_init();
+    lv_indev_init();
+
+    /*Create a Demo*/
+    lv_demo_widgets();
+    lv_demo_widgets_start_slideshow();
+
+    /*Handle LVGL tasks*/
+    while(1) {
+        lv_timer_handler();
+        usleep(5000);
+    }
+
+    return 0;
+}
